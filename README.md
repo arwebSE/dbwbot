2db9e37d8790519c7e7fe984a153eb05dfe4c6ea
Mi <mi@localhost.localdomain>
2018-09-04 13:44:40 +0200

    init github

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dbwbot
-A simple Discord bot build using Discord.py
+# DBWBot
+
+Simple open source Discord bot, built with Discord.py
+
+To install, use "sudo pip install -r requirements.txt"
+
+Before launch you must modify data/config.json.txt with your own token,
+and remove the file extension so it's only "config.json".
+
+To run the bot, launch using "python3 launch.py"
+
+Add the bot to your server using "https://discordapp.com/api/oauth2/authorize?client_id=486241051019968544&permissions=268438560&scope=bot"
---
+++ cogs/essentials.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+
+import asyncio
+
+class Essentials(discord.Client):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def studenttyp(self, ctx, role_: str):
+        """
+        Command to choose student role.
+        """
+        role_ = role_.lower()
+        role_ = role_.capitalize()
+        role = role_
+
+        member = ctx.message.author
+        role = get(member.server.roles, name=role)
+        if role is not None:
+            print(f"Selected role =", role)
+            if role_ == "Campus" or role_ == "Distans":
+                await self.bot.add_roles(member, role)
+                print(f"Added member to role.")
+            else:
+                print(f"Unauthorised role given!")
+        else:
+            print(f"Role does not exist!")
+
+def setup(bot):
+    bot.add_cog(Essentials(bot))
